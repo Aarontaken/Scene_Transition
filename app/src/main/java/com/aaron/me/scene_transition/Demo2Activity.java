8bd03484262af1c68dc917a7fd0dea067c427300
wangzhigang <wangzhigang@chunyu.me>
2016-11-10 16:05:14 +0800

    test

--- app/src/main/java/com/aaron/me/scene_transition/Demo2Activity.java
+++ app/src/main/java/com/aaron/me/scene_transition/Demo2Activity.java
@@ -9,6 +9,7 @@ public class Demo2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo2);
+
     }
 }
 
